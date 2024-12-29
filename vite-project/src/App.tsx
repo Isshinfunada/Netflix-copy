9493a0fee95e8b725f2edd05e53198e3922b4897
ぱらぽねら <138425064+Isshinfunada@users.noreply.github.com>
2024-12-29 16:25:30 +0900

    add: ジャンルごとにRowコンポーネントを表示させる

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -6,9 +6,16 @@ function App() {
    // 追加箇所
     <div className="App">
      <Row 
-          title="Netflix Originals"
-     fetchUrl={requests.fetchNetflixOriginals}
+      title="Netflix Originals" 
+      fetchUrl={requests.fetchNetflixOriginals} 
+      isLargeRow
      />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="News Movies" fetchUrl={requests.fetchNewsMovies} />
+      <Row title="Kids Movies" fetchUrl={requests.fetchKidsMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentMovies} />
     </div>
   );
 }
